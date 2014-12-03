b5b8bf4025e089dece4c89a4eb9801c59442cc9d
jbake11 <jeffbake40@gmail.com>
2014-12-02 23:18:51 -0500

    asd

--- templates/briarstone/index.php
+++ templates/briarstone/index.php
@@ -1,200 +1,171 @@
+
 <?php
-/**
- * @package     Joomla.Site
- * @subpackage  Templates.protostar
- *
- * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-defined('_JEXEC') or die;
-
-$app             = JFactory::getApplication();
-$doc             = JFactory::getDocument();
-$user            = JFactory::getUser();
-$this->language  = $doc->language;
-$this->direction = $doc->direction;
-
-// Getting params from template
-$params = $app->getTemplate(true)->params;
-
-// Detecting Active Variables
-$option   = $app->input->getCmd('option', '');
-$view     = $app->input->getCmd('view', '');
-$layout   = $app->input->getCmd('layout', '');
-$task     = $app->input->getCmd('task', '');
-$itemid   = $app->input->getCmd('Itemid', '');
-$sitename = $app->get('sitename');
-
-if($task == "edit" || $layout == "form" )
-{
-	$fullWidth = 1;
-}
-else
-{
-	$fullWidth = 0;
-}
-
-// Add JavaScript Frameworks
-JHtml::_('bootstrap.framework');
-$doc->addScript('templates/' . $this->template . '/js/template.js');
-
-// Add Stylesheets
-$doc->addStyleSheet('templates/' . $this->template . '/css/template.css');
-
-// Load optional RTL Bootstrap CSS
-JHtml::_('bootstrap.loadCss', false, $this->direction);
-
-// Adjusting content width
-if ($this->countModules('position-7') && $this->countModules('position-8'))
-{
-	$span = "span6";
-}
-elseif ($this->countModules('position-7') && !$this->countModules('position-8'))
-{
-	$span = "span9";
-}
-elseif (!$this->countModules('position-7') && $this->countModules('position-8'))
-{
-	$span = "span9";
-}
-else
-{
-	$span = "span12";
-}
-
-// Logo file or site title param
-if ($this->params->get('logoFile'))
-{
-	$logo = '<img src="' . JUri::root() . $this->params->get('logoFile') . '" alt="' . $sitename . '" />';
-}
-elseif ($this->params->get('sitetitle'))
-{
-	$logo = '<span class="site-title" title="' . $sitename . '">' . htmlspecialchars($this->params->get('sitetitle')) . '</span>';
-}
-else
-{
-	$logo = '<span class="site-title" title="' . $sitename . '">' . $sitename . '</span>';
-}
+include("head.php");
 ?>
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
-<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<jdoc:include type="head" />
-	<?php // Use of Google Font ?>
-	<?php if ($this->params->get('googleFont')) : ?>
-		<link href='//fonts.googleapis.com/css?family=<?php echo $this->params->get('googleFontName'); ?>' rel='stylesheet' type='text/css' />
-		<style type="text/css">
-			h1,h2,h3,h4,h5,h6,.site-title{
-				font-family: '<?php echo str_replace('+', ' ', $this->params->get('googleFontName')); ?>', sans-serif;
-			}
-		</style>
-	<?php endif; ?>
-	<?php // Template color ?>
-	<?php if ($this->params->get('templateColor')) : ?>
-	<style type="text/css">
-		body.site
-		{
-			border-top: 3px solid <?php echo $this->params->get('templateColor'); ?>;
-			background-color: <?php echo $this->params->get('templateBackgroundColor'); ?>
-		}
-		a
-		{
-			color: <?php echo $this->params->get('templateColor'); ?>;
-		}
-		.navbar-inner, .nav-list > .active > a, .nav-list > .active > a:hover, .dropdown-menu li > a:hover, .dropdown-menu .active > a, .dropdown-menu .active > a:hover, .nav-pills > .active > a, .nav-pills > .active > a:hover,
-		.btn-primary
-		{
-			background: <?php echo $this->params->get('templateColor'); ?>;
-		}
-		.navbar-inner
-		{
-			-moz-box-shadow: 0 1px 3px rgba(0, 0, 0, .25), inset 0 -1px 0 rgba(0, 0, 0, .1), inset 0 30px 10px rgba(0, 0, 0, .2);
-			-webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, .25), inset 0 -1px 0 rgba(0, 0, 0, .1), inset 0 30px 10px rgba(0, 0, 0, .2);
-			box-shadow: 0 1px 3px rgba(0, 0, 0, .25), inset 0 -1px 0 rgba(0, 0, 0, .1), inset 0 30px 10px rgba(0, 0, 0, .2);
-		}
-	</style>
-	<?php endif; ?>
-	<!--[if lt IE 9]>
-		<script src="<?php echo $this->baseurl; ?>/media/jui/js/html5.js"></script>
-	<![endif]-->
-</head>
+</div>
+</div>
+</div>
+<script type="text/css">
 
-<body class="site <?php echo $option
-	. ' view-' . $view
-	. ($layout ? ' layout-' . $layout : ' no-layout')
-	. ($task ? ' task-' . $task : ' no-task')
-	. ($itemid ? ' itemid-' . $itemid : '')
-	. ($params->get('fluidContainer') ? ' fluid' : '');
-?>">
+    .transition-timer-carousel .carousel-caption {
+        background: -moz-linear-gradient(top,  rgba(0,0,0,0) 0%, rgba(0,0,0,0.1) 4%, rgba(0,0,0,0.5) 32%, rgba(0,0,0,1) 100%); /* FF3.6+ */
+        background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0)), color-stop(4%,rgba(0,0,0,0.1)), color-stop(32%,rgba(0,0,0,0.5)), color-stop(100%,rgba(0,0,0,1))); /* Chrome,Safari4+ */
+        background: -webkit-linear-gradient(top,  rgba(0,0,0,0) 0%,rgba(0,0,0,0.1) 4%,rgba(0,0,0,0.5) 32%,rgba(0,0,0,1) 100%); /* Chrome10+,Safari5.1+ */
+        background: -o-linear-gradient(top,  rgba(0,0,0,0) 0%,rgba(0,0,0,0.1) 4%,rgba(0,0,0,0.5) 32%,rgba(0,0,0,1) 100%); /* Opera 11.10+ */
+        background: -ms-linear-gradient(top,  rgba(0,0,0,0) 0%,rgba(0,0,0,0.1) 4%,rgba(0,0,0,0.5) 32%,rgba(0,0,0,1) 100%); /* IE10+ */
+        background: linear-gradient(to bottom,  rgba(0,0,0,0) 0%,rgba(0,0,0,0.1) 4%,rgba(0,0,0,0.5) 32%,rgba(0,0,0,1) 100%); /* W3C */
+        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00000000', endColorstr='#000000',GradientType=0 ); /* IE6-9 */
+        width: 100%;
+        left: 0px;
+        right: 0px;
+        bottom: 0px;
+        text-align: left;
+        padding-top: 5px;
+        padding-left: 15%;
+        padding-right: 15%;
+    }
+    .transition-timer-carousel .carousel-caption .carousel-caption-header {
+        margin-top: 10px;
+        font-size: 24px;
+    }
 
-	<!-- Body -->
-	<div class="body">
-		<div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : ''); ?>">
-			<!-- Header -->
-			<header class="header" role="banner">
-				<div class="header-inner clearfix">
-					<a class="brand pull-left" href="<?php echo $this->baseurl; ?>">
-						<?php echo $logo; ?>
-						<?php if ($this->params->get('sitedescription')) : ?>
-							<?php echo '<div class="site-description">' . htmlspecialchars($this->params->get('sitedescription')) . '</div>'; ?>
-						<?php endif; ?>
-					</a>
-					<div class="header-search pull-right">
-						<jdoc:include type="modules" name="position-0" style="none" />
-					</div>
-				</div>
-			</header>
-			<?php if ($this->countModules('position-1')) : ?>
-				<nav class="navigation" role="navigation">
-					<jdoc:include type="modules" name="position-1" style="none" />
-				</nav>
-			<?php endif; ?>
-			<jdoc:include type="modules" name="banner" style="xhtml" />
-			<div class="row-fluid">
-				<?php if ($this->countModules('position-8')) : ?>
-					<!-- Begin Sidebar -->
-					<div id="sidebar" class="span3">
-						<div class="sidebar-nav">
-							<jdoc:include type="modules" name="position-8" style="xhtml" />
-						</div>
-					</div>
-					<!-- End Sidebar -->
-				<?php endif; ?>
-				<main id="content" role="main" class="<?php echo $span; ?>">
-					<!-- Begin Content -->
-					<jdoc:include type="modules" name="position-3" style="xhtml" />
-					<jdoc:include type="message" />
-					<jdoc:include type="component" />
-					<jdoc:include type="modules" name="position-2" style="none" />
-					<!-- End Content -->
-				</main>
-				<?php if ($this->countModules('position-7')) : ?>
-					<div id="aside" class="span3">
-						<!-- Begin Right Sidebar -->
-						<jdoc:include type="modules" name="position-7" style="well" />
-						<!-- End Right Sidebar -->
-					</div>
-				<?php endif; ?>
-			</div>
-		</div>
-	</div>
-	<!-- Footer -->
-	<footer class="footer" role="contentinfo">
-		<div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : ''); ?>">
-			<hr />
-			<jdoc:include type="modules" name="footer" style="none" />
-			<p class="pull-right">
-				<a href="#top" id="back-top">
-					<?php echo JText::_('TPL_PROTOSTAR_BACKTOTOP'); ?>
-				</a>
-			</p>
-			<p>
-				&copy; <?php echo date('Y'); ?> <?php echo $sitename; ?>
-			</p>
-		</div>
-	</footer>
-	<jdoc:include type="modules" name="debug" style="none" />
-</body>
-</html>
+    @media (min-width: 970px) {
+        /* Lower the font size of the carousel caption header so that our caption
+        doesn't take up the full image/slide on smaller screens */
+        .transition-timer-carousel .carousel-caption .carousel-caption-header {
+            font-size: 36px;
+        }
+    }
+    .transition-timer-carousel .carousel-indicators {
+        bottom: 0px;
+        margin-bottom: 5px;
+    }
+    .transition-timer-carousel .carousel-control {
+        z-index: 11;
+    }
+    .transition-timer-carousel .transition-timer-carousel-progress-bar {
+        height: 5px;
+        background-color: #5cb85c;
+        width: 0%;
+        margin: -5px 0px 0px 0px;
+        border: none;
+        z-index: 11;
+        position: relative;
+    }
+    .transition-timer-carousel .transition-timer-carousel-progress-bar.animate{
+        /* We make the transition time shorter to avoid the slide transitioning
+        before the timer bar is "full" - change the 4.25s here to fit your
+        carousel's transition time */
+        -webkit-transition: width 4.25s linear;
+        -moz-transition: width 4.25s linear;
+        -o-transition: width 4.25s linear;
+        transition: width 4.25s linear;
+    }
+
+</script>
+
+
+<div class='underNavIndex clearfix hidden-lg hidden-md hidden-sm'>
+    <br /><br />
+</div>
+
+<!--id='sliderWrite'-->
+
+<div  class="container-fluid" >
+    <div class="row ">
+        <!-- The carousel -->
+        <div id="transition-timer-carousel" class=" carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#transition-timer-carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#transition-timer-carousel" data-slide-to="1"></li>
+                <li data-target="#transition-timer-carousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img class='img-responsive center-block' src="/images/slider/panel_1.jpg" />
+                </div>
+
+                <div class="item">
+                    <img class='img-responsive center-block' src="/images/slider/panel_2.jpg" />
+                </div>
+
+                <div class="item">
+                    <img class='img-responsive center-block' src="/images/slider/panel_3.jpg" />
+                </div>
+            </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#transition-timer-carousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+            </a>
+            <a class="right carousel-control" href="#transition-timer-carousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+
+            <!--             Timer "progress bar"
+                        <hr class="transition-timer-carousel-progress-bar animate" />-->
+        </div>
+    </div>
+</div>
+
+<div class="container indexContent">
+    <div class="row">
+        <div class="col-lg-9 col-md-9 col-sm-9">
+            <h1>Knowledge. Equipment. Experience.</h1>
+
+            <h3 class='textAdjust'>From single machining operations service to full manufacturing capabilities,
+                you can count on Instrumental Machine & Development.</h3>
+
+            Instrument Machine & Development (IMD) is a quality minded manufacturer of Orthopedic and
+            Medical Instrumentation, supporting the Orthopedic community.
+
+            IMD is committed to quality, on-time delivery and meeting the requirements of each and every customer.
+
+            IMD specializes in Orthopedic Instrumentation, Sterilization Trays and accessories for the Operating Room.
+            We have the knowledge and experience to serve our customers manufacturing and assembly needs.
+
+        </div>
+        <div class='col-lg-3 col-md-3 col-sm-3'>
+            <a href='history.php'><img class='img-responsive' src='images/company-history.jpg'></a>
+        </div>
+    </div>
+</div>
+
+<div class='clearfix'></div>
+<br /><br />
+
+<div class='container indexContent'>
+    <div class='row'>
+        <div class='col-sm-4'>
+            <ul>
+                <li id='equipment'><a href='equip.php'>Equipment</a></li>
+            </ul>
+            IMD utilizes the latest technology for orthopedic tool production. We offer a full line of in-house production mills, lathes, metal markers, cutting equipment and finishers. If specialized tool applications are required, we have the resources to complete the job to your specifications and satisfaction.
+        </div>
+        <div class='col-sm-4'>
+            <ul>
+                <li id='products'><a href='photo.php'>Product Samples</a></li>
+            </ul>
+            We've been supplying the orthopedic industry with surgical instrumentation since 1991. Over the years the surgical procedures have advanced and become less invasive thanks to the advancements in surgical instrument engineering and production processes. Visit our gallery of view the various tools we have produced.
+        </div>
+        <div class='col-sm-4'>
+            <ul>
+                <li id='employment'><a href='employ.php'>Employment</a></li>
+            </ul>
+            IMD is an equal opportunity employer. We seek out candidates that share our vision for excellence and drive for customer satisfaction. If you would like to become part of the IMD family, please contact us for more information on available positions.
+        </div>
+    </div>
+</div>
+
+<div class='container'>
+    <div class='row'>
+        <div class='col-lg-12'>
+            <?php
+            include("foot.php");
+            ?>
+
+
+
