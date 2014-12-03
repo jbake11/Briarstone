f01ec84cae8260f1f28aab8249bc98007ec33e08
Chris Bake <cbake7806@gmail.com>
2014-12-03 00:32:46 -0500

    debug

--- templates/briarstone/index.php
+++ templates/briarstone/index.php
@@ -52,10 +52,10 @@ $sitename = $app->get('sitename');
 
         <div class="container">
             <div class="row">
-                <div class="col-lg-2 col-md-2 col-sm-6">
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
                     <jdoc:include type="modules" name="logo" style="xhtml" />
                 </div>
-                <div class="col-lg-10 col-md-10 col-sm-6">
+                <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
                     <jdoc:include type="modules" name="mainmenu" style="xhtml" />
                 </div>
             </div>
