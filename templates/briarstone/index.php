fcee705e694292470a0c3c98240e2d57be4c3158
jbake11 <jeffbake40@gmail.com>
2014-12-04 17:20:58 -0500

    asd

--- templates/briarstone/index.php
+++ templates/briarstone/index.php
@@ -106,7 +106,7 @@ $sitename = $app->get('sitename');
 
     </div>
 
-    <div id='footer' class="container-fluid image-responsive" >
+    <div id='footer' class="container-fluid img-responsive" >
         <div class="container">
             <div class="row" >
                 <div class='col-lg-3 col-md-3 col-sm-6 col-xs-6'>
