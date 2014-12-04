e5171b75dbe3b8fb23a359ff846d4c0940528193
jbake11 <jeffbake40@gmail.com>
2014-12-04 16:59:56 -0500

    asd

--- templates/Briarstone/css/scratch2.php
+++ templates/Briarstone/css/scratch2.php
@@ -6,3 +6,8 @@
     homes in many areas across
     South Eastern Michigan such as...
 </div>
+
+
+<h2>Briarstone Building &amp; Residential Realty</h2>
+<h3>Briarstone Building has based its reputation on Innovation, Integrity and Service.</h3>
+<p>Through this process clients turn into repeat customers. Clients are continuously referring Briarstone to their friends and family. From the layout and design phase to warranty – Briarstone Building has out paced the competition. By implementing years of construction experience and quality control, we continually complete our client's projects on time and exceed their expectations. We keep a thumb on the pulse of the realty market as well. If you’re looking for that “fixer upper”, we can help you find it, work out the best purchase price and remodel it to reflect your style.</p>
