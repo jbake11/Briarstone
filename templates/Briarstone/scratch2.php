30e37cc6f42eb58b5ee342c960f8149d49f15a4e
jbake11 <jeffbake40@gmail.com>
2014-12-05 21:18:53 -0500

    asd

---
+++ templates/Briarstone/scratch2.php
@@ -0,0 +1,10 @@
+<div class="footTitle">About Us</div>
+<div class="footText">
+    The company was founded to build
+    homes from 2000 square feet to
+    5000 square feet. He has built
+    homes in many areas across
+    South Eastern Michigan such as...
+</div>
+
+
