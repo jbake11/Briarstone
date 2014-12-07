e240294a7d415bf73db3e805f9d175916264a64c
jbake11 <jeffbake40@gmail.com>
2014-12-07 00:02:07 -0500

    asd

--- templates/Briarstone/scratch2.php
+++ templates/Briarstone/scratch2.php
@@ -1,10 +1,11 @@
-<div class="footTitle">About Us</div>
-<div class="footText">
-    The company was founded to build
-    homes from 2000 square feet to
-    5000 square feet. He has built
-    homes in many areas across
-    South Eastern Michigan such as...
+<div class="row">
+    <div class="col-lg-4">
+        <img class="img-responsive" src="/templates/Briarstone/images/drew.jpg" />
+        <br /><br />
+        <img class="img-responsive" src="/templates/Briarstone/images/bio_house.jpg" />
+    </div>
+    <div class="col-lg-8">
+        <h2>About the Builder</h2>
+        Drew Duhn grew up in Beverly Hills, Michigan and graduated Birmingham Groves High School. He obtained the rank of Eagle Scout with the Boy Scouts of America. Drew graduated with a Bachelors Degree in Construction Management from Purdue University. After graduation from college he worked as a project manager for new home and condominium builders. Drew founded Briarstone Building in 1996. The company was founded to build homes from 2000 square feet to 5000 square feet. He has built homes in many areas across South Eastern Michigan such as; Northville, Novi, Canton, West Bloomfield, Sterling Heights, Troy, Franklin, and Wixom. Upon the demand of past clients who insisted that Drew finish their basement, he decided to branch out. Now Briarstone Building specializes in finished basements, renovations, and remodeling existing kitchens and bathrooms. Drew has over 19 years of experience, which encompasses many facets of the real estate industry. He is also a licensed Real Estate Broker in the State of Michigan. Drew has been married to Gabriella for 17 years and is father to Dominic (12) and Enzo (10).
+    </div>
 </div>
-
-
