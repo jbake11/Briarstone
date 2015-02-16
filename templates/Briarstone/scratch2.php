3d0e1677ea28f5eb58231a99d703c7e12e32b3ff
jbake11 <jeffbake40@gmail.com>
2015-02-15 22:30:26 -0500

    a link color

--- templates/Briarstone/scratch2.php
+++ templates/Briarstone/scratch2.php
@@ -1,33 +1,37 @@
 <div class="row">
-    <div class="col-sm-6 col-md-6 col-lg-6 col-lg-12">
-        <h2>Services</h2>
-        <br />
-        Whether it is a new house, kitchen, basement, bathroom, or insurance repair.
-        Briarstone Building is able to handle all of your remodeling and new construction needs.
-        From design phase to completion – Briarstone Building offers Turn Key construction.
-        We are licensed and insured to handle any project. Briarstone Building will take your
-        simple or complicated construction project and complete it in a fast, friendly, and competive manner.
-        Our quality of construction and eye for detail always shows through when completed.
-        <br />
-        <ul>
-            <li>Home plan design</li>
-            <li>New homes</li>
-            <li>Remodeling</li>
-            <li>Additions</li>
-            <li>Bathrooms</li>
-            <li>Kitchens</li>
-            <li>Finished basements</li>
-            <li>Walkout egress steps and windows</li>
-            <li>Exterior water infiltration (brick, wood shingles and flashing)</li>
-            <li>Insurance work (non emergency)</li>
-        </ul>
-        <br />
-        We also offer realty services to find the home that fits your lifestyle. Briarstone Realty, Inc.
-        is a full service real estate company. We are able to find you a house or lot as a Buyer's Agent
-        or sell your current home as a Seller's Agent.
-
-    </div>
-    <div class="col-sm-6 col-md-6 col-lg-6 col-lg-12">
-        <img class="img-responsive" src="/templates/Briarstone/images/services_photo.jpg" />
+    <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
+        <h2>Testimonials</h2>
     </div>
 </div>
+<p> </p>
+<div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
+        Briarstone Realty, Inc is a full service real estate company. We are equipped to find you the house or lot that fits your lifestyle. As a Buyer’s Agent, we can sell your current home while we are building your new home.
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
+        Keeping within budget is very important for any home buyer. We will search your preferred area and come up with as many listings as possible that meet your criteria.
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
+        Getting the most for your home as a seller is equally important. With Briarstone Building’s decades of building experiece, we can come into your home and make recommendations on updates needed to make sure you get the maximum value for your home.
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
+        To start your search for a new home be sure to visit <a fref="http://www.moveinmichigan.com/">MoveInMichigan.com</a> for the latest MLS listings.
+    </div>
+</div>
+<br />
+
+
+
+
+
+
+
