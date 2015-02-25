378cb2b0d71e8798e7be10ddcf2af3eba48bd8e5
jbake11 <jeffbake40@gmail.com>
2015-02-25 10:16:40 -0500

    asd

--- templates/Briarstone/scratch2.php
+++ templates/Briarstone/scratch2.php
@@ -16,6 +16,25 @@
     </div>
 </div>
 <br />
+<div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
+        <h3>Video Title</h3>
+        <!--Place Embed Code between here-->
+
+        <!--Place Embed Code between here-->
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
+        <h3>Video Title</h3>
+        <!--Place Embed Code between here-->
+
+        <!--Place Embed Code between here-->
+    </div>
+</div>
+<br />
+
 
 
 
