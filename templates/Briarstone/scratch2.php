42df8a63f230368bca501b639ad300af6579a372
jbake11 <jeffbake40@gmail.com>
2015-02-25 09:42:50 -0500

    nav

--- templates/Briarstone/scratch2.php
+++ templates/Briarstone/scratch2.php
@@ -1,29 +1,25 @@
 <div class="row">
     <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
-        <a href="/briarstone-realty"><img src="images/realtor.jpg" alt="" /></a>
+        <h3>Video Title</h3>
+        <!--Place Embed Code between here-->
+
+        <!--Place Embed Code between here-->
     </div>
 </div>
 <br />
 <div class="row">
     <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
-        We will be glad to meet with you to discuss building options and advantages. From design to finished product, you will have the peace of mind knowing your new home or remodel project will be completed to your satisfaction.
-        <br /><br />
-        Give us a call or email today!
+        <h3>Video Title</h3>
+        <!--Place Embed Code between here-->
+
+        <!--Place Embed Code between here-->
     </div>
 </div>
 <br />
-<div class="row">
-    <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
-        <p><b>Briarstone Building, Inc.</b></p>
-        <p>46709 Merion Circle</p>
-        <p>Northville, Michigan 48167</p>
-        <br />
-        <p>Phone: 248.535.3838</p>
-        <p>Fax: 248.692.0921</p>
-        <p>Email: drew@briarstonebuilding.com</p>
-    </div>
-</div>
-<br />
+
+
+
+
 
 
 
